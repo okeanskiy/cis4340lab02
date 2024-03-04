754d78a1f1c4e51b1ff5ef50fc507dd8e5e44fb1
Adam Cunningham <ytadamforeva3@gmail.com>
2024-03-03 23:03:11 -0500

    Create cis4340a.java

---
+++ cis4340a.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+ *  Compilation:  javac Cis4340a.java
+ *  Execution:    java Cis4340a
+ *
+ *  Prints 
+ *    "Hello, World - from the class of CIS4340 - Spring 2024 -UCF _Adam Cunningham_".
+ *  By tradition, this is everyone's first program.
+ *
+ * Revision History:
+ *   % java Cis4340a
+ *  Hello, World - from the class of CIS4340 - Spring 2024 - UCF
+ *  %
+ *
+ ******************************************************************************/
+
+public class Cis4340a {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World - from the class of CIS4340 - Fall 2022 - UCF");
+    }
+
+}
